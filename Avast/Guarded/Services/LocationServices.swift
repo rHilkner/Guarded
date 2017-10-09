26b08a9c26d1d7d822e525cc2c1e3b960701b2af
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2017-10-09 16:22:56 -0300

    Adding LocationServices and TimerServices classes

---
+++ Avast/Guarded/Services/LocationServices.swift
@@ -0,0 +1,28 @@
+//
+//  LocationServices.swift
+//  Guarded
+//
+//  Created by Rodrigo Hilkner on 09/10/17.
+//  Copyright © 2017 Rodrigo Hilkner. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationServices {
+    
+    ///Gets user's location
+    func getLocation() -> CLLocation {
+        
+    }
+    
+    ///Sends user's location to another user
+    func sendLocation(location: CLLocation, user: User) {
+        
+    }
+    
+    ///Receives location from another user
+    func receiveLocation(location: CLLocation, user: User) {
+        
+    }
+}
---
+++ Avast/Guarded/Services/TimerServices.swift
@@ -0,0 +1,26 @@
+//
+//  TimerServices.swift
+//  Guarded
+//
+//  Created by Rodrigo Hilkner on 09/10/17.
+//  Copyright © 2017 Rodrigo Hilkner. All rights reserved.
+//
+
+import Foundation
+
+class TimerServices {
+    ///Sets timer to a given time interval
+    func setTimer(time: TimeInterval) {
+        
+    }
+    
+    ///Called every second to reduce timer's counting by 1 second
+    func updateTimer() {
+        
+    }
+    
+    ///Adds time interval to timer
+    func addTime(time: TimeInterval) {
+        
+    }
+}
