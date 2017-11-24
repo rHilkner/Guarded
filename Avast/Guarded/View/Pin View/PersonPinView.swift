12265fda5cfa7ce16b62998990c20e6bd99996e2
Unknown <fi.marques33@gmail.com>
2017-11-24 06:45:36 -0200

    New logo and annotations
    AppIcon update and creation of custom annotationViews

---
+++ Avast/Guarded/View/Pin View/PersonPinView.swift
@@ -0,0 +1,13 @@
+//
+//  PersonPinView.swift
+//  Guarded
+//
+//  Created by Filipe Marques on 24/11/17.
+//  Copyright © 2017 Rodrigo Hilkner. All rights reserved.
+//
+
+import UIKit
+
+class PersonPinView: NSObject {
+
+}
